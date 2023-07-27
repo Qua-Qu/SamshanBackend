2401b87c0902593669cccd2c712f91c7822c3464
Didi Aryadi <108334135+Qua-Qu@users.noreply.github.com>
2023-07-28 02:49:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: "https://samshanbackend-production.up.railway.app:3000",
+    origin: "samshanbackend-production.up.railway.app:3000",
     credentials: true,
   })
 );
 
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "https://samshanbackend-production.up.railway.app:3000",
+    origin: "samshanbackend-production.up.railway.app:3000",
     credentials: true,
   },
 });
