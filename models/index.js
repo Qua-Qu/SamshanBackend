71c8984e0fa2afe4a421d26e69c28c1a3559281a
Didi Aryadi <108334135+Qua-Qu@users.noreply.github.com>
2023-07-28 06:38:53 +0700

    se

--- models/index.js
+++ models/index.js
@@ -4,15 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 const basename = path.basename(__filename)
-const config = require(__dirname + '/config.json')[env];
+const config = require(__dirname + '/../config.json')[env];
 const db = {};
 
-if (config[env].use_env_variable) {
-  sequelize = new Sequelize(process.env[config[env].use_env_variable], config[env]);
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config[env].database, config[env].username, config[env].password, config[env]);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 
