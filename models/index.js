f1ac7eb129a5bdeb6e88d742f666118576917054
Didi Aryadi <108334135+Qua-Qu@users.noreply.github.com>
2023-07-28 05:56:54 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const env = process.env.NODE_ENV || 'development';
 const basename = path.basename(__filename)
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json');
 const db = {};
 
 const sequelize = new Sequelize(config[env].url, {
